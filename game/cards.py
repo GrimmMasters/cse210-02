0d2cf5c7d4c51a5528c988c0afe917255e568a7d
GrimmMasters <mcc21006@byui.edu>
2022-05-05 18:47:13 -0600

    Updated

---
+++ game/cards.py
@@ -0,0 +1,7 @@
+# initial function must start player with 300 pts
+# initial function must display a random card from 1-13 and set to some variable
+#
+# 
+# definition where the next card found
+# compare old number to new number with high or low
+# deal out points
--- game/hilo.py
+++ game/hilo.py
@@ -1,1 +1,4 @@
-print("Hello World")
+# Setup initial variables
+# Start game function
+# Check if player wants to continue playing
+# have player 
