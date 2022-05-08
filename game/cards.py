4faec05784717d519c1fec9f8c4eaac5b90c91fe
GrimmMasters <mcc21006@byui.edu>
2022-05-07 18:18:09 -0600

    Initial Push for card game

--- game/cards.py
+++ game/cards.py
@@ -1,7 +1,19 @@
-# initial function must start player with 300 pts
+
 # initial function must display a random card from 1-13 and set to some variable
 #
 # 
 # definition where the next card found
 # compare old number to new number with high or low
 # deal out points
+import random
+
+class Die:
+
+    def __init__(self):
+        self.value = 0
+        self.points = 0
+
+    def roll(self):
+
+        self.value = random.randint(1,6)
+        self.points = 50 if self.value == 5 else 100 if self.value == 1 else 0
--- game/hilo.py
+++ game/hilo.py
@@ -2,3 +2,55 @@
 # Start game function
 # Check if player wants to continue playing
 # have player 
+# initial function must start player with 300 pts
+from game.cards import Card
+
+
+class Hilo:
+    def __init__(self):
+
+        self.dice = []
+        self.is_playing = True
+        self.score = 0
+        self.total_score = 0
+
+        for i in range(5):
+            die = Die()
+            self.dice.append(die)
+
+    def start_game(self):
+
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+
+        roll_dice = input("Roll dice? [y/n] ")
+        self.is_playing = (roll_dice == "y")
+       
+    def do_updates(self):
+
+        if not self.is_playing:
+            return 
+
+        for i in range(len(self.dice)):
+            die = self.dice[i]
+            die.roll()
+            self.score += die.points 
+        self.total_score += self.score
+
+    def do_outputs(self):
+
+        if not self.is_playing:
+            return
+        
+        values = ""
+        for i in range(len(self.dice)):
+            die = self.dice[i]
+            values += f"{die.value} "
+
+        print(f"You rolled: {values}")
+        print(f"Your score is: {self.total_score}\n")
+        self.is_playing == (self.score > 0)
