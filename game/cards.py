8fd887b2934b2121f9374885ce9251e51f9fd17a
GrimmMasters <mcc21006@byui.edu>
2022-05-10 17:42:20 -0600

    Update Cards.py

--- game/cards.py
+++ game/cards.py
@@ -5,28 +5,58 @@
 # definition where the next card found
 # compare old number to new number with high or low
 # deal out points
+
 import random
 
 class Card:
+  def __init__(self):
+    
+    self.card1 = 0
+    self.card2 = 0
+    self.guess = ""
+    self.result = ""
+    
+    
+    
+  def h_l(self):
+    self.card1 = random.randint(1, 13)
+    self.card2 = random.randint(1, 13)
+    print(f"\nThe card is: {self.card1}")
+    guess = input("Higher or lower? [h/l] ")
+    print(f"Next card was: {self.card2}")
+    
+    if self.card1 < self.card2 and guess == "h":
+      self.result = "correct"
+    elif self.card1 > self.card2 and guess == "h":
+      self.result = "incorrect"
+    elif self.card1 > self.card2 and guess == "l":
+      self.result = "correct"
+    else:
+      self.result = "incorrect"
 
-    def __init__(self):
-        self.value = 0
-        self.points = 0
+
+# import random
+
+# class Card:
+
+#     def __init__(self):
+#         self.value = 0
+#         self.points = 0
 
 
-    def deal(self):
+#     def deal(self):
 
-        self.value = random.randint(1,13)
+#         self.value = random.randint(1,13)
 
-    def points(self, choice, Old_value):
+#     def points(self, choice, Old_value):
 
-        if choice == 'h' and Old_value < self:
-            self.points = 100
-        elif choice == 'l' and Old_value > self:
-            self.points = 100
-        elif choice == 'l' or'h' and Old_value == self:
-            self.points = 0
-        else:
-            self.points = -75
+#         if choice == 'h' and Old_value < self:
+#             self.points = 100
+#         elif choice == 'l' and Old_value > self:
+#             self.points = 100
+#         elif choice == 'l' or'h' and Old_value == self:
+#             self.points = 0
+#         else:
+#             self.points = -75
 
 
