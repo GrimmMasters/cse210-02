7e2f98d0fa1e27e39e72c95a0d91db0ee4bb8383
GrimmMasters <mcc21006@byui.edu>
2022-05-10 17:46:16 -0600

    Update hilo

--- game/hilo.py
+++ game/hilo.py
@@ -7,18 +7,20 @@
 #Alexander and Pume on initial and input
 
 
-from game.cards import Card
-import random
 
 
-class Hilo:
-    def __init__(self):
+# from game.cards import Card
+# import random
 
-        self.Card = []
-        self.is_playing = True
-        self.score = 0
-        self.total_score = 0
-        self.old_value = 0
+
+# class Hilo:
+#     def __init__(self):
+
+#         self.Card = []
+#         self.is_playing = True
+#         self.score = 0
+#         self.total_score = 0
+#         self.old_value = 0
 
 #Start game by dealing out first card 
         # card = list(range(0,14))
@@ -27,38 +29,38 @@ class Hilo:
         #     print (f"The card is: {card}")
 
 
-    def start_game(self):
+    # def start_game(self):
 
-        while self.is_playing:
-            self.get_inputs()
-            self.do_outputs()
-            self.do_updates()
+    #     while self.is_playing:
+    #         self.get_inputs()
+    #         self.do_outputs()
+    #         self.do_updates(self, self.guess, self.old_value)
 
-    def get_inputs(self):
+    # def get_inputs(self):
 
-        # self.guess = input("Higher or lower? [h/l] ")
-        deal_card = input("Play again? [y/n] ")
-        self.is_playing = (deal_card == "y")
+    #     self.guess = input("Higher or lower? [h/l] ")
+    #     deal_card = input("Play again? [y/n] ")
+    #     self.is_playing = (deal_card == "y")
        
-    def do_updates(self, choice, old_value):
+    # def do_updates(self, choice, old_value):
 
-        if not self.is_playing:
-            return 
-        card = self.Card
-        card.points()
-        self.score += card.points 
-        self.total_score += self.score
+    #     if not self.is_playing:
+    #         return 
+    #     card = self.Card
+    #     card.points(self, choice, old_value)
+    #     self.score += card.points 
+    #     self.total_score += self.score
 
-    def do_outputs(self):
+    # def do_outputs(self):
 
-        if not self.is_playing:
-            return
+    #     if not self.is_playing:
+    #         return
         
-        values = ""
-        for i in range(len(self.dice)):
-            die = self.dice[i]
-            values += f"{die.value} "
+    #     values = ""
+    #     for i in range(len(self.dice)):
+    #         die = self.dice[i]
+    #         values += f"{die.value} "
 
-        print(f"You rolled: {values}")
-        print(f"Your score is: {self.total_score}\n")
-        self.is_playing == (self.score > 0)
+    #     print(f"You rolled: {values}")
+    #     print(f"Your score is: {self.total_score}\n")
+    #     self.is_playing == (self.score > 0)
