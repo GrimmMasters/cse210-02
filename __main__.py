a82aaed7107351fcf2e49157a358fd59847e66c3
garretts8 <gar21085@byui.edu>
2022-05-10 18:10:59 -0600

    Adding inital main info

---
+++ __main__.py
@@ -0,0 +1,11 @@
+from game.hilo import Hilo
+hilo = Hilo()
+# Card.h_l()
+ 
+# hilo.give_points()
+
+hilo.game_in_play(hilo.give_points())
+
+# Hilo()
+# print(hilo)
+# hilo.game_in_play(hilo.give_points(Card.h_l()))l
